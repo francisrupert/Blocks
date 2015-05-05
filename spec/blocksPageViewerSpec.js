3c8f89d3b002c426f17cd40a78eee95ee87c2796
Kevin Powell <kevin@eightshapes.com>
2015-05-05 08:40:57 -0500

    Filter out BPV changes from babel-traceur swap changes

--- spec/blocksPageViewerSpec.js
+++
@@ -1,6 +0,0 @@
-console.log(BlocksPageViewer);
-describe("BlocksPageViewer", function(){
-	it("should load BlocksConfig", function(){
-		expect(window.BlocksPageViewer.do_stuff()).toEqual(5);
-	});
-});
