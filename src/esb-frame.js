30c6a306887ed54ac8c293f10eac6330b5bde416
Kevin Powell <kevin@eightshapes.com>
2015-07-10 08:17:25 -0400

    WIP: Frame refactor to prep for responsive frame

--- src/esb-frame.js
+++ src/esb-frame.js
@@ -107,7 +107,6 @@ export class EsbFrame {
 		return option_value;
 	}
 
-	// Determine early if this is a standard FRAME for a page or a COMPONENT FRAME
 	get_component_frame_status() {
 		var self = this,
 			is_component_frame = false,
@@ -131,7 +130,7 @@ export class EsbFrame {
 			'title': false,
 			'caption': false,
 			'dimensions': true,
-			'href': 'none',
+			'href': false,
 			'scrolling': 'no',
 			'overlay': true,
 			'scale': false,
@@ -157,7 +156,6 @@ export class EsbFrame {
 		return options; 
 	}
 
-	// BOTH - CORE - Config - Mixed, refactor?
 	get_frame_options() {
 		var self = this,
 			options = self.default_options,
@@ -212,9 +210,9 @@ export class EsbFrame {
 		options.iframe_src = self.build_iframe_src(options);
 
 		//HREF
-		if (options.href === 'none') {
+		if (options.href === false && self.is_option_overridden('href') === false) {
 			// href wasn't set at any level, default to options.frame
-			options.href = self.iframe_src;
+			options.href = options.iframe_src;
 		}
 
 		return options;
@@ -288,23 +286,56 @@ export class EsbFrame {
 	}
 
 // PLACEHOLDER ELEMENT CONSTRUCTION
+
+	/* FIVE MAIN CONTAINING ELEMENTS:
+	.esb-frame outermost wrapper
+		.esb-frame-inner-wrap, either a link or a span wrapping all of the contents within
+			.esb-frame-title
+			.esb-frame-caption
+			.esb-frame-device-annotation
+			.esb-frame-dimensions-annotation
+			.esb-frame-iframe-wrap, wrapper around the iframe related content - can be used to crop the iframe content - not scaled
+				.esb-frame-device, <svg> element absolutely positioned within .esb-frame-iframe-wrap as a background for the <iframe>
+				.esb-loading-animation
+				.esb-frame-iframe-inner-wrap, wrapper around the iframe and any other scaled content - IS scaled
+					.esb-frame-iframe, the <iframe> element itself 
+	*/
 	// BOTH - CORE
 	create_placeholder_element() {
-		var self = this;
+		var self = this,
+			outer_wrap = self.get_element_outer_wrap(),
+			inner_wrap = self.get_element_inner_wrap(),
+			title = self.get_element_title(),
+			caption = self.get_element_caption(),
+			dimensions_annotation = self.get_element_dimensions_annotation(),
+			device_annotation = self.get_element_device_annotation(),
+			iframe_wrap = self.get_element_iframe_wrap();
 
-		self.placeholder_element = '<div class="esb-frame ';
-		if (self.options.overlay) { self.placeholder_element += ' esb-frame-has-overlay '; }
-		if (self.options['device-frame']) { self.placeholder_element += ' esb-frame--has-device-frame esb-frame-device-frame-' + self.options['viewport-device']; }
-		if (self.options['viewport-device-orientation'] === 'landscape') { self.placeholder_element += ' esb-frame-device-orientation-landscape'; }
-		if (self.is_component_frame) { self.placeholder_element += ' esb-frame--is-framed-component'; }
-		self.placeholder_element += '" '; 
-		self.placeholder_element +='data-esb-uuid="' + self.uuid + '">';
-		if (self.options.href) { 
-			self.placeholder_element += '<a class="esb-frame-link esb-frame-inner-wrap" href="' + self.options.href + '">'; 
-		}
-		else {
-			self.placeholder_element += '<span class="esb-frame-inner-wrap">'; 
-		}
+		// EsbUtil.addClass(placeholder_element, 'esb-frame');
+		// placeholder_element.setAttribute('data-esb-uuid', self.uuid);
+
+		// if (self.options.overlay) { EsbUtil.addClass(placeholder_element, 'esb-frame-has-overlay'); }
+		// if (self.is_component_frame) { EsbUtil.addClass(placeholder_element, ' esb-frame--is-framed-component'); }
+		// if (self.options['device-frame']) { 
+		// 	EsbUtil.addClass(placeholder_element, 'esb-frame--has-device-frame esb-frame-device-frame-' + self.options['viewport-device']); 
+		// 	EsbUtil.addClass(placeholder_element, 'esb-frame-device-orientation-' + self.options['viewport-device-orientation']); 
+		// }
+
+
+
+
+		// self.placeholder_element = '<div class="esb-frame ';
+		// if (self.options.overlay) { self.placeholder_element += ' esb-frame-has-overlay '; }
+		// if (self.options['device-frame']) { self.placeholder_element += ' esb-frame--has-device-frame esb-frame-device-frame-' + self.options['viewport-device']; }
+		// if (self.options['viewport-device-orientation'] === 'landscape') { self.placeholder_element += ' esb-frame-device-orientation-landscape'; }
+		// self.placeholder_element += '" '; 
+		// self.placeholder_element +='data-esb-uuid="' + self.uuid + '">';
+		// if (self.options.href) { 
+		// 	self.placeholder_element += '<a class="esb-frame-link esb-frame-inner-wrap" href="' + self.options.href + '">'; 
+		// }
+		// else {
+		// 	self.placeholder_element += '<span class="esb-frame-inner-wrap">'; 
+		// }
 		self.placeholder_element += self.get_title();
 		self.placeholder_element += self.get_caption();
 		self.placeholder_element += self.get_dimensions_annotation();
@@ -318,31 +349,121 @@ export class EsbFrame {
 		self.placeholder_element += '</div>';
 	}
 
-	// BOTH - FEATURE
-	get_title() {
+	get_element_outer_wrap() {
 		var self = this,
-			title = '';
+			outer_wrap = document.createElement('div');
+
+		EsbUtil.addClass(outer_wrap, 'esb-frame');
+		outer_wrap.setAttribute('data-esb-uuid', self.uuid);
+
+		if (self.options.overlay) { EsbUtil.addClass(outer_wrap, 'esb-frame-has-overlay'); }
+		if (self.is_component_frame) { EsbUtil.addClass(outer_wrap, ' esb-frame--is-framed-component'); }
+		if (self.options['device-frame']) { 
+			EsbUtil.addClass(outer_wrap, 'esb-frame--has-device-frame esb-frame-device-frame-' + self.options['viewport-device']); 
+			EsbUtil.addClass(outer_wrap, 'esb-frame-device-orientation-' + self.options['viewport-device-orientation']); 
+		}
+
+		return outer_wrap;
+	}
+
+	get_element_inner_wrap() {
+		var self = this,
+			inner_wrap;
+
+		if (self.options.href) {
+			inner_wrap = document.createElement('a');
+			EsbUtil.addClass(inner_wrap, 'esb-frame-link');
+			inner_wrap.setAttribute('href', self.options.href);
+		}
+		else {
+			inner_wrap = document.createElement('span');
+		}
+
+		EsbUtil.addClass(inner_wrap, 'esb-frame-inner-wrap');
+
+		return inner_wrap;
+	}
+
+	// BOTH - FEATURE
+	get_element_title() {
+		var self = this,
+			title;
+
 		if (self.options.title) {
-			title = '<h3 class="esb-frame-title">' + self.options.title + '</h3>';
+			title = document.createElement('h3');
+			title.textContent = self.options.title;
+			EsbUtil.addClass(title, 'esb-frame-title');
 		}
 
 		return title;
 	}
 
 	// BOTH - FEATURE
-	get_caption() {
+	get_element_caption() {
 		var self = this,
-			caption = '';
+			caption;
 		if (self.options.caption) {
-			caption = '<p class="esb-frame-caption">' + self.options.caption + '</p>';
+			caption = document.createElement('p');
+			caption.textContent = self.options.caption;
+			EsbUtil.addClass(caption, 'esb-frame-caption');
 		}
 
 		return caption;
 	}
 
+	get_element_device_annotation() {
+		var self = this,
+			device_annotation,
+			annotation_text;
+
+		if (self.options['viewport-device'] && self.options['device-annotation']) {
+			device_annotation = document.createElement('p');
+			annotation_text = self.device_presets[self.options['viewport-device']]['annotation-name'];
+			if (self.options['viewport-device-orientation'] === 'landscape') {
+				annotation_text += ', Landscape';
+			}
+			device_annotation.textContent = annotation_text;
+			EsbUtil.addClass(device_annotation, 'esb-frame-device-annotation');
+		}
+
+		return device_annotation;
+	}
+
+	// BOTH - CORE
+	get_element_dimensions_annotation() {
+		var self = this,
+			dimensions = self.get_iframe_dimensions(),
+			dimensions_annotation,
+			scale = parseFloat((dimensions.scale*100).toFixed(1));
+		
+		// if (self.options['viewport-device'] && self.options['device-annotation']) {
+		// 	dimensions_annotation += '<p class="esb-frame-device-annotation">';
+		// 	dimensions_annotation += self.device_presets[self.options['viewport-device']]['annotation-name'];
+		// 	if (self.options['viewport-device-orientation'] === 'landscape') {
+		// 		dimensions_annotation += ', Landscape';
+		// 	}
+		// 	dimensions_annotation += '</p>';
+		// }
+
+		if (self.options.dimensions && dimensions.width && dimensions.height && dimensions.scale) {
+			dimensions_annotation = document.createElement('p');
+			EsbUtil.addClass(dimensions_annotation, 'esb-frame-dimensions-annotation esb-frame-dimensions--updating');
+	// TODO START HERE WITH: EsbUtil.get_svg_icon('dimensions')
+			dimensions_annotation += self.get_element_dimensions_icon() + '<span class="esb-frame-dimensions-value"><span class="esb-frame-dimensions-width-value">' + Math.round(dimensions.width) + '</span>&times;<span class="esb-frame-dimensions-height-value">' + Math.round(dimensions.height) + '</span></span> ';
+			if (scale !== 100) {
+				dimensions_annotation += self.get_element_scale_icon() + '<span class="esb-frame-scale-value">' + parseFloat((dimensions.scale*100).toFixed(1)) + '%</span>';
+			}
+			dimensions_annotation += '</p>';
+		}
+
+		return dimensions_annotation;
+	}
+
 	// BOTH - FEATURE - UTIL
 	get_scale_icon() {
 		var scale_icon = '<span class="esb-frame-scale-icon">';
+// TODO START HERE WITH: EsbUtil.get_svg_icon('dimensions')
+
 		scale_icon += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 13.973" height="15px">';
 		scale_icon += '<g><path d="M8.361,7.749c-0.043,0-0.077,0.005-0.113,0.012c-0.02,0.002-0.039,0.014-0.051,0.014 C8.177,7.783,8.154,7.788,8.14,7.794C8.116,7.802,8.1,7.815,8.084,7.825C8.068,7.831,8.051,7.841,8.036,7.848 c-0.061,0.044-0.115,0.099-0.16,0.16C7.869,8.022,7.858,8.039,7.854,8.056c-0.012,0.02-0.027,0.033-0.03,0.055 C7.814,8.13,7.812,8.148,7.802,8.171C7.799,8.185,7.792,8.2,7.787,8.219C7.783,8.256,7.775,8.294,7.776,8.335v3.296 c0,0.327,0.262,0.587,0.585,0.587c0.322,0,0.585-0.26,0.585-0.587V9.743l4.059,4.058c0.226,0.229,0.595,0.229,0.822,0 c0.23-0.229,0.23-0.599,0-0.824l-4.06-4.06h1.893c0.158,0.001,0.308-0.062,0.414-0.172c0.103-0.106,0.167-0.249,0.167-0.41 c0-0.326-0.26-0.586-0.581-0.586H8.361z"/><path d="M6.42,0H0.584C0.262,0,0,0.261,0,0.583v5.835c0,0.319,0.262,0.581,0.584,0.581H6.42 C6.738,6.999,7,6.737,7,6.418V0.583C7,0.261,6.738,0,6.42,0z M1.17,1.168h4.662v4.665H1.17V1.168z"/></g></svg>';
 		scale_icon += '</span>';
@@ -351,7 +472,7 @@ export class EsbFrame {
 	}
 
 	// BOTH - FEATURE - UTIL
-	get_dimensions_icon() {
+	get_element_dimensions_icon() {
 		var dimensions_icon = '<span class="esb-frame-dimensions-icon">';
 		dimensions_icon += '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 13.993" height="15px">';
 		dimensions_icon += '<path d="M13.996,3.487c0-0.034-0.002-0.067-0.006-0.096c-0.006-0.021-0.01-0.039-0.017-0.054 c-0.007-0.02-0.009-0.041-0.019-0.056c-0.006-0.021-0.018-0.04-0.029-0.056c-0.007-0.015-0.014-0.032-0.025-0.047 c-0.016-0.028-0.041-0.055-0.062-0.077c-0.004-0.005-0.006-0.01-0.008-0.011l0,0l0,0l-2.91-2.922 c-0.226-0.226-0.594-0.226-0.824-0.003c-0.228,0.229-0.228,0.6-0.002,0.826l1.919,1.926L3.499,2.914l0,0 c-0.153,0-0.302,0.062-0.412,0.172C2.978,3.194,2.917,3.342,2.917,3.5l0.006,8.491l-1.928-1.928c-0.226-0.232-0.595-0.232-0.824,0 c-0.228,0.224-0.229,0.592-0.001,0.82l2.931,2.939c0.109,0.109,0.259,0.17,0.416,0.17c0.162,0,0.301-0.061,0.411-0.17l2.899-2.926 c0.228-0.232,0.225-0.602-0.001-0.828c-0.231-0.225-0.601-0.225-0.828,0.008l-1.911,1.928L4.084,4.08l7.924,0.008l-1.921,1.914 c-0.231,0.224-0.232,0.594-0.004,0.821c0.113,0.115,0.263,0.174,0.413,0.174c0.149,0,0.297-0.058,0.41-0.174l2.924-2.908l0,0 c0.027-0.027,0.051-0.058,0.07-0.086c0.012-0.014,0.018-0.031,0.025-0.047c0.012-0.021,0.021-0.035,0.028-0.056 c0.011-0.02,0.013-0.036,0.02-0.06c0.007-0.015,0.011-0.03,0.017-0.05C13.994,3.582,14,3.542,14,3.501l0,0 C14,3.499,13.996,3.489,13.996,3.487z"/></svg>';
@@ -360,33 +481,6 @@ export class EsbFrame {
 		return dimensions_icon;
 	}
 
-	// BOTH - CORE
-	get_dimensions_annotation() {
-		var self = this,
-			dimensions = self.get_iframe_dimensions(),
-			dimensions_annotation = '',
-			scale = parseFloat((dimensions.scale*100).toFixed(1));
-		
-		if (self.options['viewport-device'] && self.options['device-annotation']) {
-			dimensions_annotation += '<p class="esb-frame-device-annotation">';
-			dimensions_annotation += self.device_presets[self.options['viewport-device']]['annotation-name'];
-			if (self.options['viewport-device-orientation'] === 'landscape') {
-				dimensions_annotation += ', Landscape';
-			}
-			dimensions_annotation += '</p>';
-		}
-
-		if (self.options.dimensions && dimensions.width && dimensions.height && dimensions.scale) {
-			dimensions_annotation += '<p class="esb-frame-dimensions-annotation esb-frame-dimensions--updating">';
-			dimensions_annotation += self.get_dimensions_icon() + '<span class="esb-frame-dimensions-value"><span class="esb-frame-dimensions-width-value">' + Math.round(dimensions.width) + '</span>&times;<span class="esb-frame-dimensions-height-value">' + Math.round(dimensions.height) + '</span></span> ';
-			if (scale !== 100) {
-				dimensions_annotation += self.get_scale_icon() + '<span class="esb-frame-scale-value">' + parseFloat((dimensions.scale*100).toFixed(1)) + '%</span>';
-			}
-			dimensions_annotation += '</p>';
-		}
-
-		return dimensions_annotation;
-	}
 
 	// BOTH  - CORE - Rename!
 	get_iframe_wrap() {
