7a1c6ee68ebedf3d21baf3c701c7bc4ea6dd6412
Kevin Powell <kevin@eightshapes.com>
2015-08-20 08:49:18 -0500

    WIP: Continued scrub of 'component'

--- src/esb-page.js
+++ src/esb-page.js
@@ -9,7 +9,6 @@ class EsbPage {
     var self = this;
 
     self.logger = EsbUtil.logger;
-    self.timer = EsbUtil.timer();
     self.blocks_done = false;
     self.blocks_done_timeout_ms = 15000;
 
@@ -18,24 +17,6 @@ class EsbPage {
     self.parsed_esb_marks = [];
     self.esb_mark_auto_id = 1;
 
-    // page cache of components
-    self.components = {};
-    self.component_variations = {};
-    self.cache = {};
-    self.time_start = self.timer();
-    self.time_duration = null;
-
-    // Keep track of kids purely to know when the page is finished rendering
-    // in order to fire off JS at the end and trigger a page done event
-    self.child_count = 0;
-    self.children_loaded = 0;
-    self.children_rendered = 0;
-    self.child_count_js = 0;
-    self.child_js_injected = 0;
-
-    // A flag for children to know that there are no more parents to notify
-    // The 'page' type is the root
-    self.type = 'page';
     self.setEventListeners();
   }
 
@@ -280,122 +261,6 @@ class EsbPage {
     }
 
     return include_snippet;
-  }
-
-  /**
-   * @method: childDoneLoading
-   * @params: child
-   *
-   * Takes a EsbComponent object, tracks that it is finished loading,
-   * and calls render on that object.
-   *
-   * This function is recursive in that it will render children
-   * nested inside this child.
-   */
-  childDoneLoading(child) {
-    var self = this;
-
-    self.children_loaded++;
-
-    self.logger('debug', 'READY TO RENDER PAGE LEVEL CHILDREN: ' + child.template_name());
-
-    child.render();
-  }
-
-  /**
-   * @method: childDoneRendering
-   * @params: child
-   *
-   * Takes a EsbComponent object, tracks that it has finished rendering
-   * itself.
-   * Replaces components on the page with their child's rendered elements.
-   * Then injects the component Javascript.
-   */
-  childDoneRendering(child) {
-    var self = this,
-      $page_component;
-
-
-    if (child.content !== undefined) {
-      $page_component = self.$root.find('[data-component="' + child.name + '"][data-variation="' + child.variation_name + '"][data-content=\'' + child.content + '\']');
-    }
-    else {
-      $page_component = self.$root.find('[data-component="' + child.name + '"][data-variation="' + child.variation_name + '"]').not('[data-content]');
-    }
-
-    self.children_rendered++;
-
-    self.logger('debug', 'READY TO RENDER PAGE LEVEL Component: ' + child.template_name());
-
-    if (child.replace_reference || child.frame_with_documentation) {
-      $page_component.replaceWith(child.$el);
-    } else {
-      $page_component.append(child.$el);
-    }
-
-    // Once all of the kids are done we'll spawn all JS
-    if (self.child_count === self.children_rendered) {
-      self.injectComponentJS();
-    }
-
-    // Exposing just the page level component variations
-    // to pages using Blocks
-    self.component_variations[child.template_name()] = child;
-  }
-
-  getIDFromVariation($component) {
-    return $component.attr('data-blocks-uuid');
-  }
-
-  injectComponentJS() {
-    var self = this;
-
-    for (var name in self.components) {
-      let component = self.components[name];
-      component.injectJS(self);
-      self.child_count_js++;
-    }
-  }
-
-  childDoneInjectingJS() {
-    var self = this,
-        event;
-
-    self.child_js_injected++;
-
-    if (self.child_count_js === self.child_js_injected) {
-      if (window.self !== window.top) {
-        // If blocks is being run inside an iFrame (Blocks Viewer)
-        self.logger('debug', 'TRIGGERING blocks-done on parent body from within iFrame');
-        parent.$('body').trigger('blocks-done');
-
-        self.logger('debug', 'TRIGGERING blocks-done-inside-viewer on parent body from within iFrame');
-        parent.$('body').trigger('blocks-done-inside-viewer', {'iframe_id': window.frameElement.id});
-
-        // This triggers blocks-done within the iFrame itself. BlocksViewer has a listener for this event so the height and width of the iframe can be dynamically set after BlocksLoader has finished
-        $('body').trigger('blocks-done');
-      }
-      else {
-        // Blocks loader is being used without BlocksViewer
-        self.logger('info', 'TRIGGERING blocks-done');
-        $(document).trigger('blocks-done');
-      }
-
-      // Trigger non-jQuery version of the blocks-done event
-      if (window.CustomEvent) {
-        event = new CustomEvent('blocks-done');
-      } else {
-        event = document.createEvent('CustomEvent');
-        event.initCustomEvent('blocks-done', true, true);
-      }
-
-      document.dispatchEvent(event);
-
-      self.setBlocksDone();
-
-      self.time_duration = self.timer() - self.time_start;
-      self.logger('info', 'TOTAL DURATION: ' + self.time_duration);
-    }
   }
 
   setBlocksDone() {
--- src/esb.js
+++ src/esb.js
@@ -3,8 +3,8 @@ import EsbPage from './esb-page';
 import EsbUtil from './esb-util';
 
 EsbConfig.load().then(function() {
-  EsbPage.renderIncludeSnippetFromQueryStringParams(); //Used by Frame to generate a component snippet from query string params
-	EsbPage.parse(); //Finds all blocks components, viewers, etc. and preps them for loading/display
+  EsbPage.renderIncludeSnippetFromQueryStringParams(); //Used by Frame to generate a include snippet from query string params
+	EsbPage.parse(); //Finds all blocks includes, viewers, etc. and preps them for loading/display
 	EsbPage.display();
   EsbPage.blocksDone().then(
     function(){
